922aceeeb883875c9834adf3c4ab9f15872f219b
OscillatingBlock <aayusharma270506@gmail.com>
2025-06-30 19:54:41 +0530

    internal/paste/repository.go and internal/user/repository.go

--- internal/paste/repository.go
+++ internal/paste/repository.go
@@ -9,7 +9,8 @@ import (
 
 type PasteRepository interface {
 	Create(ctx context.Context, paste *Paste) error
-	GetById(ctx context.Context, id string) (*Paste, error)
+	GetByID(ctx context.Context, id string) (*Paste, error)
+	Update(ctx context.Context, paste *Paste) error
 }
 
 type pasteRepository struct {
@@ -23,20 +24,29 @@ func NewPasteRepository(db *bun.DB) *pasteRepository {
 }
 
 func (r *pasteRepository) Create(ctx context.Context, paste *Paste) error {
-	_, err := r.db.NewCreateTable().Model(paste).Exec(ctx)
+	_, err := r.db.NewInsert().Model(paste).Exec(ctx)
 	if err != nil {
-		slog.Error("Error while inserting paste", "pasteid", paste.ID)
+		slog.Error("Error while inserting paste", "operation", "Create", "pasteid", paste.ID, "error", err)
 		return err
 	}
 	return nil
 }
 
-func (r *pasteRepository) GetById(ctx context.Context, id string) (*Paste, error) {
+func (r *pasteRepository) GetByID(ctx context.Context, id string) (*Paste, error) {
 	var paste Paste
-	err := r.db.NewSelect().Model(&paste).Where("id = ?", id).Scan(ctx)
+	err := r.db.NewSelect().Model(&paste).Where("id = ? AND expires_at > NOW()", id).Scan(ctx)
 	if err != nil {
 		slog.Error("Error while getting paste", "pasteid", id, "error", err)
 		return nil, err
 	}
 	return &paste, nil
+}
+
+func (r *pasteRepository) UpdatePaste(ctx context.Context, paste *Paste) error {
+	_, err := r.db.NewUpdate().Model(paste).Where("id = ?").Exec(ctx)
+	if err != nil {
+		slog.Error("Error while updating paste", "operation", "update", "pasteid", paste.ID, "error", err)
+		return err
+	}
+	return nil
 }
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,53 @@
 package user
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/uptrace/bun"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *User) error
+	GetByID(ctx context.Context, id string) (*User, error)
+	GetByPublicKey(ctx context.Context, public_key string) (*User, error)
+}
+
+type userRepository struct {
+	db *bun.DB
+}
+
+func NewUserRepositry(db *bun.DB) *userRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (r *userRepository) Create(ctx context.Context, user *User) error {
+	_, err := r.db.NewInsert().Model(user).Exec(ctx)
+	if err != nil {
+		slog.Error("Error while inserting user", "operation", "create", "userid", user.ID)
+		return err
+	}
+	return nil
+}
+
+func (r *userRepository) GetByID(ctx context.Context, id string) (*User, error) {
+	var user User
+	err := r.db.NewSelect().Model(&user).Where("id = ?", id).Scan(ctx)
+	if err != nil {
+		slog.Error("Error while getting user", "operation", "get", "userID", id, "error", err)
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) GetByPublicKey(ctx context.Context, public_key string) (*User, error) {
+	var user User
+	err := r.db.NewSelect().Model(&user).Where("public_key = ?", public_key).Scan(ctx)
+	if err != nil {
+		slog.Error("Error while getting user", "opearation", "get", "public_key", public_key[:8], "error", err)
+		return nil, err
+	}
+	return &user, nil
+}
