c1e4fb2bbc350e74047669d3eb2edc6044eeb6cd
OscillatingBlock <aayusharma270506@gmail.com>
2025-07-05 17:58:58 +0530

    add paste handlers

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24.4
 require (
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/google/uuid v1.6.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.13.4
 	github.com/stretchr/testify v1.10.0
 	github.com/uptrace/bun v1.2.14
 	github.com/uptrace/bun/dialect/mysqldialect v1.2.14
@@ -15,12 +17,20 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/mod v0.25.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/paste/handler.go
+++ internal/paste/handler.go
@@ -1,1 +1,107 @@
 package paste
+
+import (
+	"net/http"
+	"os"
+
+	"Drop-Key/internal/models"
+
+	"github.com/labstack/echo/v4"
+)
+
+type PasteHandler struct {
+	service PasteService
+}
+
+type PasteRequest struct {
+	Ciphertext string `json:"ciphertext"`
+	Signature  string `json:"signature"`
+	PublicKey  string `json:"publickey"`
+	Expires_in int    `json:"expiresin"`
+}
+
+type PasteResponse struct {
+	Ciphertext string `json:"ciphertext"`
+	Signature  string `json:"signature"`
+	PublicKey  string `json:"publickey"`
+}
+
+func (h *PasteHandler) CreatePaste(c echo.Context) error {
+	pasteReq := &PasteRequest{}
+	err := c.Bind(pasteReq)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Internal server error")
+	}
+	paste := &models.Paste{
+		Signature:  pasteReq.Signature,
+		Ciphertext: pasteReq.Ciphertext,
+		PublicKey:  pasteReq.PublicKey,
+	}
+
+	ctx := c.Request().Context()
+	id, err := h.service.Create(ctx, paste, pasteReq.Expires_in)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal server error")
+	}
+
+	url := getUrl(id, paste.PublicKey)
+	return c.JSON(http.StatusOK, url)
+}
+
+func getUrl(id, pub string) string {
+	baseUrl := os.Getenv("BASEURL")
+	if baseUrl == "" {
+		baseUrl = "https://yourpastebin.com"
+	}
+	url := baseUrl + "/paste/" + id + "#" + pub
+	return url
+}
+
+func (h *PasteHandler) GetPaste(c echo.Context) error {
+	var id string
+	id = c.Param("id")
+	if id == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "missing paste ID")
+	}
+
+	ctx := c.Request().Context()
+	paste, err := h.service.GetByID(ctx, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal server error")
+	}
+
+	response := PasteResponse{
+		Ciphertext: paste.Ciphertext,
+		Signature:  paste.Signature,
+		PublicKey:  paste.PublicKey,
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
+
+func (h *PasteHandler) UpdatePaste(c echo.Context) error {
+	id := c.Param("id")
+	if id == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "missing paste ID")
+	}
+
+	pasteReq := &PasteRequest{}
+	if err := c.Bind(pasteReq); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid input")
+	}
+
+	paste := &models.Paste{
+		ID:         id,
+		Signature:  pasteReq.Signature,
+		Ciphertext: pasteReq.Ciphertext,
+		PublicKey:  pasteReq.PublicKey,
+	}
+
+	ctx := c.Request().Context()
+	err := h.service.Update(ctx, paste)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal server error, faield to update paste.")
+	}
+
+	return c.String(http.StatusOK, "paste updated")
+}
