c394a1d324c9e522b7cd1c1a08c501c15456e651
OscillatingBlock <aayusharma270506@gmail.com>
2025-07-09 00:14:30 +0530

    Wired all stuff

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,31 @@ import (
 	"log/slog"
 
 	"Drop-Key/internal/db"
+	"Drop-Key/internal/paste"
+	"Drop-Key/internal/router"
+	"Drop-Key/internal/user"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("ERROR Failed to get DSN from .env file.")
+	}
 	ctx := context.Background()
 	db, err := db.InitDB(ctx)
 	if err != nil {
 		slog.Error("Error initialising database.", "error", err)
 	}
+	pasteRepo := paste.NewPasteRepository(db)
+	userRepo := user.NewUserRepository(db)
+	pasteService := paste.NewPasteService(pasteRepo, userRepo)
+	userService := user.NewUserService(userRepo)
+
+	pasteHandler := paste.NewPasteHandler(pasteService)
+	userHandler := user.NewUserHandler(userService)
+
+	router := router.Router(pasteHandler, userHandler)
+
+	router.Logger.Fatal(router.Start("127.0.0.1:8080"))
 }
